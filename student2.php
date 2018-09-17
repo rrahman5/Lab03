20430d3712ffc40b4767825da295035109647a98
Jonathan Roman <jroman8@u.rochester.edu>
2018-09-17 16:16:53 -0400

    student2 update (victoria)

--- student2.php
+++ student2.php
@@ -4,25 +4,23 @@
 <header>
 	<div class="header">
 		<h3 class="studentName">Victoria Bongard</h3>
-		<img src="" alt="" class="photo">
+		<img src="images/victoria.jpg" alt="victoria" class="photo">
 	</div>
 </header>
 <?php include "inc/nav.inc"; ?>
 
 	<div class="contentBlock">
-		<p></p>
+		<p>In terms of my academics at the University of Rochester, I have found my study places to be among that which I consider most important to my academic success. I tend to prefer mostly quiet, cozy places to study. In general, I find a loud and crowded space to be too distracting when trying to accomplish work. My favorite study space on campus is Rettner because of the natural lighting, bright colors, and generally quiet atmosphere. It is also very close to much of campus, which is a big bonus.</p>
+		<p>While the new iZone has not been open for long, so far I have found it to be a really nice study area. I also enjoy the Reference Stacks in Rush Rhees, Robinson Library, and the Art and Music Library since both are warm and quiet. I find Carlson, the Stacks, Q and I, and the first floor of Morey to be less conducive to studying, though they are still acceptable places. At the bottom of the totem pole I place Gleason because it is too loud and distracting for me to possibly be productive.</p>
 	</div>
 
 	<div class="contentBlock">
-		<p></p>
+		<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+		<p>During my second year at college I began three new positions. First, I got the job of a Study Zone Leader, which is a peer academic support position run through the Center for Excellence in Teaching and Learning. My second position was as a paid notetaker, meaning I take notes for any student(s) who may be unable to do so. This position is managed by the Office for Disability Resources. The third position was a Meridian, meaning I give tours and assist the admissions office at the University of Rochester. Though it keeps me busy, I still maintain all three of these positions and just this semester I was promoted to the position of Head Study Zone Leader.</p>
 	</div>
 
 	<div class="contentBlock">
-		<p></p>
-	</div>
-
-	<div class="contentBlock">
-		<p></p>
+		<p>The next logical step in my journey is figuring out my post-grad plans. While I still have lots to figure out, and lots of time to do it, I have been thinking recently about what exactly I would like to do and where I would like to be. I know for a fact that I want to live and work somewhere with winters that are noticeably warmer than those in Rochester. I am interested in film, media, web design, public relations, and marketing, which are skills that I think can be combined easily. I would love to work in media marketing and helping companies grow their brand.</p>
 	</div>
 
 <script src="http://code.jquery.com/jquery.js"></script>
