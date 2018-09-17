e6598b50c3a5d657bc8fadf5f647757813abd320
Jonathan Roman <jroman8@u.rochester.edu>
2018-09-17 16:22:27 -0400

    student3 update (jayda)

--- student3.php
+++ student3.php
@@ -3,26 +3,25 @@
 <body>
 <header>
 	<div class="header">
-		<h3 class="studentName">Jayda Medina</h3>
-		<img src="" alt="" class="photo">
+		<h1 class="studentName">Jayda Medina</h1>
+		<img src="images/jayda.jpg" alt="jayda" class="photo">
 	</div>
 </header>
 <?php include "inc/nav.inc"; ?>	
 
 	<div class="contentBlock">
-		<p></p>
+		<h2>Classes</h2>
+		<p>During my first semester at the University of Rochester, I took Beginning American Sign Language, CSC170, CSC161, and MTH140. That semester I made Dean’s List and was invited to join the National Society of Collegiate Scholars. The following semester I took CSC171, MTH150, Beginning American Sign Language II, and WRT105 RAGE!. My humanities cluster is American Sign Language and I’m still deciding my Social Science cluster. I am currently deciding between Gender and Women’s studies and Anthropology. This semester I’m taking Intro to Medical Anthropology, which can be used towards either cluster. I’m doing a human and computer interactions track, with the goal of getting a Bachelor of Arts for Computer Science.</p>
 	</div>
 
 	<div class="contentBlock">
-		<p></p>
+		<h2>Clubs</h2>
+		<p>When I’m not in class, or studying, I’m doing one of my many extra curriculars. Last semester, some friends and I started Students for Chronic Illness Visibility. This is our first semester as a SA recognized club. We are devoted to spreading awareness about chronic illnesses and promoting accessibility and proper accommodations on campus. One of our goals this year is to create a lending library, where people can borrow braces, mobility aids, and anything else that might getting around campus with a chronic illness easier. We are also working towards getting students with disabilities priority in U of R’s housing lottery. Outside of SCIV, I’m also a member of Strong Jugglers and TOOP. Strong Jugglers is a circus arts club where we perform juggling, flow arts, balancing acts, and other circus arts. TOOP is a student run theatre company. </p>
 	</div>
 
 	<div class="contentBlock">
-		<p></p>
-	</div>
-
-	<div class="contentBlock">
-		<p></p>
+		<h2>Jobs</h2>
+		<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. During my last week I assisted a photography counselor with a different group of kids. Before that job, I had a paid part in the play Sistas on Fire, a show about the hardships African American women face in America. My first job was an activities specialist for Sesame Flyers. There I taught java to 6th graders during an after school program. Outside of work, I’ve done many hours of community service. In high school I started a Toys for Tots drive. I’ve also done campaign work in a mayoral election. At Grace Cathedral I worked as an Audiovisual board operator.</p>
 	</div>
 
 <script src="http://code.jquery.com/jquery.js"></script>
