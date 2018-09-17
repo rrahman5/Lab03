db2baa9583fcbfa6ef579e04253af885e23dfb76
Jonathan Roman <jroman8@u.rochester.edu>
2018-09-17 16:14:37 -0400

    homepage update

--- index.php
+++ index.php
@@ -3,24 +3,27 @@
 <body>
 <header>
 	<div class="header">
-		<h3 class="studentName"></h3>
+		<h1 class="studentName">Homepage</h1>
 	</div>
 </header>
 <?php include "inc/nav.inc"; ?>
 		
 	<div class="studentProfile">
-		<img src="" alt="" class="photo">
-		<p></p>
+		<h2>Jonathan Roman</h2>
+		<img src="images/jonathan.jpg" alt="jonathan" class="photo">
+		<p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.</p>
 	</div>
 
 	<div class="studentProfile">
-		<img src="" alt="" class="photo">
-		<p></p>
+		<h2>Victoria Bongard</h2>
+		<img src="images/victoria.jpg" alt="victoria" class="photo">
+		<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. My family moved to Bloomington, Indiana during the summer before I started high school. Two months after moving I began attending Chatham Hall, an all-girls boarding school located in rural southern Virginia. While my family has lived in Indiana for the past 6 years, I have only ever spent breaks there. Thus, I consider myself to be from Virginia and occasionally Indiana, too.</p>
 	</div>
 
 	<div class="studentProfile">
-		<img src="" alt="" class="photo">
-		<p></p>
+		<h2>Jayda Medina</h2>
+		<img src="images/jayda.jpg" alt="jayda" class="photo">
+		<p>I was born in Manhattan, NY to an immigrant mother from Guyana and a Puerto Rican father. I grew up in Brooklyn, NY. I went to Hebrew Educational Society for pre-kindergarten. Afterwards, I went down the street to P.S.115 for kindergarten - fifth grade. For Junior High I went to I.S.211. In eighth grade I got into Academy for Young Writers, and that’s where I spent the rest of grade school. Recently I moved from Brooklyn, NY to Long Island, but most of my family still lives in Brooklyn. I’ve visited my mother’s home country of Guyana, and the culture is a lot different than it is in America. However, I have never visited Puerto Rican. I am now a student at the University of Rochester.</p>
 	</div>
 
 <script src="http://code.jquery.com/jquery.js"></script>
