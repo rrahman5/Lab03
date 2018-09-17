66ca84817fd1112c4840f9ec05b8c768f4060d64
Jonathan Roman <jroman8@u.rochester.edu>
2018-09-17 16:09:08 -0400

    student2 update

--- student2.php
+++ student2.php
@@ -14,7 +14,7 @@
 		
 
 	<div class="contentBlock">
-		<p></p>
+		<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. My family moved to Bloomington, Indiana during the summer before I started high school. Two months after moving I began attending Chatham Hall, an all-girls boarding school located in rural southern Virginia. While my family has lived in Indiana for the past 6 years, I have only ever spent breaks there. Thus, I consider myself to be from Virginia and occasionally Indiana, too.</p>
 	</div>
 
 	<div class="contentBlock">
