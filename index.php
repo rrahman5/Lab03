b8a40e9e230191ef600cfd3d2daf66a840855e54
Jonathan Roman <jroman8@u.rochester.edu>
2018-09-17 15:53:20 -0400

    Lab 03 IA

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Lab 03 - Website</title>
+	<link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+	<div class="header">
+		<h3 class="studentName"></h3>
+	</div>
+		
+
+	<div class="studentProfile">
+		<img src="" alt="" class="photo">
+		<p></p>
+	</div>
+
+	<div class="studentProfile">
+		<img src="" alt="" class="photo">
+		<p></p>
+	</div>
+
+	<div class="studentProfile">
+		<img src="" alt="" class="photo">
+		<p></p>
+	</div>
+
+<script src=""> </script>
+</body>
+
+
+</html>
---
+++ student3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>Lab 03 - Website</title>
+	<link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+	<div class="header">
+		<h3 class="studentName">Jayda Medina</h3>
+		<img src="" alt="" class="photo">
+	</div>
+		
+
+	<div class="contentBlock">
+		<p></p>
+	</div>
+
+	<div class="contentBlock">
+		<p></p>
+	</div>
+
+	<div class="contentBlock">
+		<p></p>
+	</div>
+
+	<div class="contentBlock">
+		<p></p>
+	</div>
+
+<script src=""> </script>
+</body>
+</html>
