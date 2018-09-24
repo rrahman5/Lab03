668e9027e567d8ebd7af25d0bda4faa7a6e1a779
sharfuz95 <sshifat@u.rochester.edu>
2018-09-24 16:31:21 -0400

    first change - IA

--- index.php
+++ index.php
@@ -8,27 +8,10 @@
 </header>
 <?php include "inc/navigation.inc"; ?>
 		
-	<div class="studentProfile">
-		<h2>Jonathan Roman</h2>
-		<figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
-		<p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. I currently attend the University of Rochester with a major in Computer Science. MY hobbies include playing video games and basketball, both of which I participate in groups around campus for.</p>
-		<a href="http://csc174.org/assignment02/jroman8/Assignment02/">Jonathan's Assignment02</a>
-	</div>
-
-	<div class="studentProfile">
-		<h2>Victoria Bongard</h2>
+<figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
 		<figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
-		<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. My family moved to Bloomington, Indiana during the summer before I started high school. Two months after moving I began attending Chatham Hall, an all-girls boarding school located in rural southern Virginia. While my family has lived in Indiana for the past 6 years, I have only ever spent breaks there. Thus, I consider myself to be from Virginia and occasionally Indiana, too.</p>
+	<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
 
-		<a href="http://csc174.org/assignment02/vbongard/assignment02/home.php">Victoria's Assignment02</a>
-	</div>
-
-	<div class="studentProfile">
-		<h2>Jayda Medina</h2>
-		<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
-		<p>I was born in Manhattan, NY to an immigrant mother from Guyana and a Puerto Rican father. I grew up in Brooklyn, NY. I went to Hebrew Educational Society for pre-kindergarten. Afterwards, I went down the street to P.S.115 for kindergarten - fifth grade. For Junior High I went to I.S.211. In eighth grade I got into Academy for Young Writers, and that’s where I spent the rest of grade school. Recently I moved from Brooklyn, NY to Long Island, but most of my family still lives in Brooklyn. I’ve visited my mother’s home country of Guyana, and the culture is a lot different than it is in America. However, I have never visited Puerto Rican. I am now a student at the University of Rochester.</p>
-		<a href="http://csc174.org/assignment02/jmedina/">Jayda's Assignment02</a>
-	</div>
 
 <script src="http://code.jquery.com/jquery.js"></script>
 <script src="js/menu-highlighter.js"></script>
--- student3.php
+++ student3.php
@@ -8,8 +8,15 @@
 </header>
 <?php include "inc/navigation.inc"; ?>	
 
-	<div class="contentBlock">
+<div class="studentProfile">
+		<h2>Jayda Medina</h2>
 		<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
+		<p>I was born in Manhattan, NY to an immigrant mother from Guyana and a Puerto Rican father. I grew up in Brooklyn, NY. I went to Hebrew Educational Society for pre-kindergarten. Afterwards, I went down the street to P.S.115 for kindergarten - fifth grade. For Junior High I went to I.S.211. In eighth grade I got into Academy for Young Writers, and that’s where I spent the rest of grade school. Recently I moved from Brooklyn, NY to Long Island, but most of my family still lives in Brooklyn. I’ve visited my mother’s home country of Guyana, and the culture is a lot different than it is in America. However, I have never visited Puerto Rican. I am now a student at the University of Rochester.</p>
+		<a href="http://csc174.org/assignment02/jmedina/">Jayda's Assignment02</a>
+	</div>
+
+
+	<div class="contentBlock">
 		<h2>Classes</h2>
 		<p>During my first semester at the University of Rochester, I took Beginning American Sign Language, CSC170, CSC161, and MTH140. That semester I made Dean’s List and was invited to join the National Society of Collegiate Scholars. The following semester I took CSC171, MTH150, Beginning American Sign Language II, and WRT105 RAGE!. My humanities cluster is American Sign Language and I’m still deciding my Social Science cluster. I am currently deciding between Gender and Women’s studies and Anthropology. This semester I’m taking Intro to Medical Anthropology, which can be used towards either cluster. I’m doing a human and computer interactions track, with the goal of getting a Bachelor of Arts for Computer Science.</p>
 	</div>
