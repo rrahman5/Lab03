198195160516a39fe0de101fe68b6c42a9bc83e3
sharfuz95 <sshifat@u.rochester.edu>
2018-09-26 15:40:56 -0400

    IA update

--- index.php
+++ index.php
@@ -8,9 +8,26 @@
 </header>
 <?php include "inc/navigation.inc"; ?>
 		
+<h1> Employment </h1>
+
 <figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
-		<figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
+<p>Throughout high school I would work as a part-time tennis coach, though mostly had very light hours (about 4-5 hours a week). The hours were fairly flexible and I could pick up lessons whenever my local tennis club was teaching them, allowing me to change my schedule regularly as needed. At the time I was on the varsity tennis team so the only prerequisite I needed was to be able to play and have the patience to deal with children aged anywhere from 6-14. This was my first job I received on my own as well, which was different but educational for a 15-year-old me. Over the summer, I also taught at the summer camp which was a lot more demanding, as the hours would often be, besides for certain exceptions, 9am-4pm.</p>
+		<p>I have also worked as a warehouse employee for my dad, I would often be moving and packaging boxes and/or searching for items that needed to be shipped in the warehouse in order to package. Although it was not the most entertaining job in the world, I learned a lot about my dad's company and would be able to spend a lot of time working with my siblings along with my dad.</p>
+
+
+
+<figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
+<div class="contentBlock">
+		<p>The next logical step in my journey is figuring out my post-grad plans. While I still have lots to figure out, and lots of time to do it, I have been thinking recently about what exactly I would like to do and where I would like to be. I know for a fact that I want to live and work somewhere with winters that are noticeably warmer than those in Rochester. I am interested in film, media, web design, public relations, and marketing, which are skills that I think can be combined easily. I would love to work in media marketing and helping companies grow their brand.</p>
+	</div>
+	
+	
 	<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
+	<div class="contentBlock">
+		<h2>Jobs</h2>
+		<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. During my last week I assisted a photography counselor with a different group of kids. Before that job, I had a paid part in the play Sistas on Fire, a show about the hardships African American women face in America. My first job was an activities specialist for Sesame Flyers. There I taught java to 6th graders during an after school program. Outside of work, I’ve done many hours of community service. In high school I started a Toys for Tots drive. I’ve also done campaign work in a mayoral election. At Grace Cathedral I worked as an Audiovisual board operator.</p>
+	</div>
+
 
 
 <script src="http://code.jquery.com/jquery.js"></script>
