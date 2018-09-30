b126ed419d427de459f90764003848e0afe076d9
kchitap <43220337+kchitap@users.noreply.github.com>
2018-09-30 19:49:06 -0400

    added layout to student profiles

--- index.php
+++ index.php
@@ -6,34 +6,47 @@
 		
 		<div class="header">
 			
-			<h1 class="studentName">Homepage</h1>
+			<h1> Leaders of Summer Camps</h1>
 		
 		</div>
-		
+
 	</header>
 			
-	<h1> Leaders of Summer Camps</h1>
-	
-	<h2>Jonathan Roman</h2>
-	
-	<figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
-	
-	<p> Over the summer, I also taught at the summer camp which was a lot more demanding, as the hours would often be, besides for certain exceptions, 9am-4pm.</p>
-			
+	<div class="jon">
 
-
-	<h2>Victoria Bongard</h2>
-	
-	<figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
-	
-	<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+		<h2>Jonathan Roman</h2>
 		
-
-	<h2>Jayda Medina</h2>
+		<a href="student1.php">
+			<figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
+		</a>
+		
+		<p> Over the summer, I also taught at the summer camp which was a lot more demanding, as the hours would often be, besides for certain exceptions, 9am-4pm.</p>
 	
-	<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>	
+	</div>
 
-	<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. </p>
+	<div class="vic">
+
+		<h2>Victoria Bongard</h2>
+		
+		<a href="student2.php">
+			<figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
+		</a>
+		
+		<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+
+	</div>
+		
+	<div class="jay">
+
+		<h2>Jayda Medina</h2>
+
+		<a href="student3.php">
+			<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>	
+		</a>
+
+		<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. </p>
+
+	</div>
 
 
 
