734b4ae97dd617fe44c353b7ece0f153874d22e6
sharfuz95 <sshifat@u.rochester.edu>
2018-09-30 17:23:54 -0400

    summercamp update

--- index.php
+++ index.php
@@ -8,25 +8,29 @@
 </header>
 <?php include "inc/navigation.inc"; ?>
 		
-<h1> People born in America</h1>
-
+<h1> Leaders of Summer Camps</h1>
+<h2>Jonathan Roman</h2>
 <figure><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
-<p>My name is Jonathan Roman. I'm from Tenafly, NJ and was born September 6th, 1997. </p>
-		<p>Throughout high school I would work as a part-time tennis coach, though mostly had very light hours (about 4-5 hours a week). The hours were fairly flexible and I could pick up lessons whenever my local tennis club was teaching them, allowing me to change my schedule regularly as needed. At the time I was on the varsity tennis team so the only prerequisite I needed was to be able to play and have the patience to deal with children aged anywhere from 6-14. This was my first job I received on my own as well, which was different but educational for a 15-year-old me. Over the summer, I also taught at the summer camp which was a lot more demanding, as the hours would often be, besides for certain exceptions, 9am-4pm.</p>
-		<p>I have also worked as a warehouse employee for my dad, I would often be moving and packaging boxes and/or searching for items that needed to be shipped in the warehouse in order to package. Although it was not the most entertaining job in the world, I learned a lot about my dad's company and would be able to spend a lot of time working with my siblings along with my dad.</p>
+		<p> Over the summer, I also taught at the summer camp which was a lot more demanding, 
+		 as the hours would often be, besides for certain exceptions, 9am-4pm.</p>
+		
 
 
-
+		<h2>Victoria Bongard</h2>
 <figure><img src="images/victoria.jpg" alt="victoria" class="photo"></figure>
-<p>I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, 
-Virginia for most of the remainder of my childhood. 
- </p>
 
+	<div class="contentBlock">
+		<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). 
+		I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+	</div>
 	
-	<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
-<p>I was born in Manhattan, NY to an immigrant mother from Guyana and a Puerto Rican father. </p>
-<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>	<div class="contentBlock">
-		<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. During my last week I assisted a photography counselor with a different group of kids. Before that job, I had a paid part in the play Sistas on Fire, a show about the hardships African American women face in America. My first job was an activities specialist for Sesame Flyers. There I taught java to 6th graders during an after school program. Outside of work, I’ve done many hours of community service. In high school I started a Toys for Tots drive. I’ve also done campaign work in a mayoral election. At Grace Cathedral I worked as an Audiovisual board operator.</p>
+
+		<h2>Jayda Medina</h2>
+
+<figure><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>	
+
+<div class="contentBlock">
+		<p>My last job was as a Recreation Counselor for Hofstra Summer Camps. There I watched a group of fifth grade boys and eighth grade girls and led them through recreational activities. </p>
 	</div>
 
 
