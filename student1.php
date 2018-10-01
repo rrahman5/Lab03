72502d15614911080c0d4c723acbd5d489b32628
Reeshad Rahman <rrahman5@u.rochester.edu>
2018-10-01 15:11:14 -0400

    is current edit

--- student1.php
+++ student1.php
@@ -10,7 +10,7 @@
 
 		<div class="content">
 			
-			<?php include "inc/navigation.inc"; ?>
+			<?php $page = 'jonathan'; include "inc/navigation.inc"; ?>
 
 			<div class="about">
 				<figure class="profilepic"><img src="images/jonathan.jpg" alt="jonathan" class="photo"></figure>
--- student3.php
+++ student3.php
@@ -10,7 +10,7 @@
 
 		<div class="content">
 
-			<?php include "inc/navigation.inc"; ?>	
+			<?php $page = 'jayda'; include "inc/navigation.inc"; ?>	
 
 			<div class="about">
 				<figure class="profilepic"><img src="images/jayda.jpg" alt="jayda" class="photo"></figure>
